deb60d1b91bf2513b4ca2397f6686f1770620b46
JiaqiZhou <s2318871@ed.ac.uk>
2022-09-29 17:38:39 +0100

    change

---
+++ practical1.R
@@ -0,0 +1,4 @@
+a <- scan("D:/Rscript/SP/bible.txt",what="character",skip=104) ## skip contents
+n <- length(a)
+a <- a[-((n-2886):n)] ## strip license
+a <- a[-grep("[0123456789]:[0123456789]",a)] ## strip out verse numbers 
